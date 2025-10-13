3834731af0313b6281f6e7389d043fc9fd64255f
kozaken <gkozaka4@gmail.com>
2025-10-13 23:46:39 +0900

    style:userモデルのバリデーションでproviderカラムが冗長になっていたので修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :display_name, length: { maximum: 20 }, allow_blank: true
-  validates :provider, length: { maximum: 64 }, allow_blank: true
+  validates :provider, presence: true, if: -> { uid.present? }, length: { maximum: 64 }, allow_blank: true
   validates :uid, presence: true, if: -> { provider.present? }
-  validates :provider, presence: true, if: -> { uid.present? }
 end
