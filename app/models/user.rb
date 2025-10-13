408374152e69d299fbbc8915461cd9c4644bca9f
kozaken <gkozaka4@gmail.com>
2025-10-13 20:15:18 +0900

    add:userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :display_name, length: { maximum: 20 }, allow_blank: true
+  validates :provider, length: { maximum: 64 }, allow_blank: true
+  validates :uid, presence: true, if: -> { provider.present? }
+  validates :provider, presence: true, if: -> { uid.present? }
 end
