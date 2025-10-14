3a9e032d9e13bfa35df5cbeacb679190caf7d9a1
kozaken <gkozaka4@gmail.com>
2025-10-14 21:46:35 +0900

    add:stimulusのコントローラにメニュー表示時のオーバーレイの背景とメニュー部分の処理を追加

--- app/javascript/controllers/hamburger_controller.js
+++ app/javascript/controllers/hamburger_controller.js
@@ -1,9 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "hamburger","close" ]
+  static targets = [ "hamburger", "close", "overlay", "menu" ]
+
   toggle() {
     this.hamburgerTarget.classList.toggle("hidden");
     this.closeTarget.classList.toggle("hidden");
+    this.overlayTarget.classList.toggle("hidden");
+    this.menuTarget.classList.toggle("hidden");
   }
 }
