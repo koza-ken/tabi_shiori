64777f56f5ede9e96ccbc9e3c3b7f289a920f0be
kozaken <gkozaka4@gmail.com>
2025-10-15 17:08:34 +0900

    add:cardsテーブルのマイグレーションを実行しスキーマを更新する

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_15_045532) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_15_071356) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string "name", limit: 50, null: false
+    t.text "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "group_id"
+    t.index ["group_id"], name: "index_cards_on_group_id"
+    t.index ["user_id"], name: "index_cards_on_user_id"
+    t.check_constraint "user_id IS NOT NULL AND group_id IS NULL OR user_id IS NULL AND group_id IS NOT NULL", name: "cards_must_belong_to_user_or_group"
+  end
 
   create_table "groups", force: :cascade do |t|
     t.integer "created_by_user_id", null: false
@@ -44,4 +56,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_15_045532) do
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "cards", "groups"
+  add_foreign_key "cards", "users"
 end
