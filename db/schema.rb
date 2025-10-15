5421e59ab77679bb9ca1ce0efe118eb5a60e5eb6
kozaken <gkozaka4@gmail.com>
2025-10-15 18:50:58 +0900

    add:groupsテーブルに外部キーの関連つけ忘れていたので追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_15_071356) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_15_093819) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,4 +59,5 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_15_071356) do
 
   add_foreign_key "cards", "groups"
   add_foreign_key "cards", "users"
+  add_foreign_key "groups", "users", column: "created_by_user_id"
 end
