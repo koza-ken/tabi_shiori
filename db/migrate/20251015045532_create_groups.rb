a4fe6388b303f249ed64ebb34738c15d5dd6f8d4
kozaken <gkozaka4@gmail.com>
2025-10-15 14:06:06 +0900

    add:マイグレーションファイルを少し修正

--- db/migrate/20251015045532_create_groups.rb
+++ db/migrate/20251015045532_create_groups.rb
@@ -3,7 +3,7 @@ class CreateGroups < ActiveRecord::Migration[7.2]
     create_table :groups do |t|
       t.integer :created_by_user_id, null: false
       t.string :name, null: false, limit: 30
-      t.integer :invite_token, null: false, limit: 64
+      t.string :invite_token, null: false, limit: 64
       t.string :trip_name, limit: 50
       t.date :start_date
       t.date :end_date
@@ -11,9 +11,8 @@ class CreateGroups < ActiveRecord::Migration[7.2]
 
       t.timestamps null: false
     end
+    add_index :groups, :created_by_user_id
+    add_index :groups, :invite_token, unique: true
+    add_index :groups, :start_date
   end
-
-  add_index :groups, :created_by_user_id
-  add_index :groups, :invite_token, unique: true
-  add_index :start_date
 end
