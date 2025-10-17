e4baf2ce882e9730561092d24db18ef76086f917
kozaken <gkozaka4@gmail.com>
2025-10-17 15:56:43 +0900

    add:annotateを実行し

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,28 @@
+# == Schema Information
+#
+# Table name: groups
+#
+#  id                 :bigint           not null, primary key
+#  end_date           :date
+#  invite_token       :string(64)       not null
+#  name               :string(30)       not null
+#  start_date         :date
+#  trip_memo          :text
+#  trip_name          :string(50)
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  created_by_user_id :integer          not null
+#
+# Indexes
+#
+#  index_groups_on_created_by_user_id  (created_by_user_id)
+#  index_groups_on_invite_token        (invite_token) UNIQUE
+#  index_groups_on_start_date          (start_date)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (created_by_user_id => users.id)
+#
 class Group < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "created_by_user_id", inverse_of: :created_groups
   has_many :cards, dependent: :destroy
--- test/factories/group_memberships.rb
+++ test/factories/group_memberships.rb
@@ -1,3 +1,29 @@
+# == Schema Information
+#
+# Table name: group_memberships
+#
+#  id             :bigint           not null, primary key
+#  group_nickname :string(20)
+#  guest_token    :string(64)
+#  role           :string           default("member"), not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  group_id       :bigint           not null
+#  user_id        :bigint
+#
+# Indexes
+#
+#  index_group_memberships_on_group_id                     (group_id)
+#  index_group_memberships_on_group_id_and_group_nickname  (group_id,group_nickname) UNIQUE
+#  index_group_memberships_on_guest_token                  (guest_token)
+#  index_group_memberships_on_user_id                      (user_id)
+#  index_group_memberships_on_user_id_and_group_id         (user_id,group_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (group_id => groups.id)
+#  fk_rails_...  (user_id => users.id)
+#
 FactoryBot.define do
   factory :group_membership do
     
