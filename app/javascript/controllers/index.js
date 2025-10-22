d9218cd792cfe0e7c3e15e250f9efeecca07fe79
kozaken <gkozaka4@gmail.com>
2025-10-22 18:18:24 +0900

    add:clipboardコントローラをstimulusのリストに追加

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,12 +1,11 @@
 // Import and register all your controllers
 import { application } from "./application";
 
-// Manually import controllers (add more as needed)
 import HamburgerController from "./hamburger_controller";
-// Register controllers
 application.register("hamburger", HamburgerController);
 
-// Manually import controllers (add more as needed)
 import ModalController from "./modal_controller";
-// Register controllers
 application.register("modal", ModalController);
+
+import ClipboardController from "./clipboard_controller";
+application.register("clipboard", ClipboardController);
