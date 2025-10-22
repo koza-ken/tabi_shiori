ae74fd581335b4cc0124a7bd5628049757d13572
kozaken <gkozaka4@gmail.com>
2025-10-22 21:46:50 +0900

    add:ログイン後に元いたページに戻れるようdeviseの設定を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  # フレンドリーフォアーディング
+  before_action :store_user_location!, if: :storable_location?
 
   # ログイン後のリダイレクト先を設定（resouceを渡すとユーザーの属性で判別できる）
-  def after_sign_in_path_for(resource)
-    cards_path
+  # def after_sign_in_path_for(resource)
+  #   store_location_for(:user, request.fullpath) || cards_path
+  # end
+
+  private
+
+  # フレンドリーフォアーディング
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
   end
 end
