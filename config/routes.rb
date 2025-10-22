a4d57c9955135e74c1417e8bf53c60f6f7403c08
kozaken <gkozaka4@gmail.com>
2025-10-22 18:14:06 +0900

    add:グループに参加するgroups#joinのルーティングを定義（URLを生成するヘルパーも定義）

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   resources :cards, only: %i[index new create]
   resources :groups, only: %i[index show new create]
+
+  # 招待リンクからの参加
+  # asオプションで、/groups/join/:invite_tokenのURLを生成するヘルパーを定義
+  get '/groups/join/:invite_token', to: 'groups#join', as: :join_group
 end
