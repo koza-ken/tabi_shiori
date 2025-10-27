739d6443070f8a2b1faebb8ddb6b800a6f8c411e
kozaken <gkozaka4@gmail.com>
2025-10-27 13:15:47 +0900

    add:フラッシュのタイプによって色を使い分けるようヘルパーを作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def flash_style_classes(type)
+    case type.to_s
+    when "notice"
+      "bg-green-500"
+    when "alert"
+      "bg-red-500"
+    when "error"
+      "bg-red-500"
+    else
+      "bg-blue-500"
+    end
+  end
 end
