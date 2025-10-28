d11307b5425d511f41a43580839fb33c0dcbfa0e
kozaken <gkozaka4@gmail.com>
2025-10-28 13:09:05 +0900

    add:categoryモデルに関連とバリデーションを追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -13,4 +13,8 @@
 #  index_categories_on_display_order  (display_order) UNIQUE
 #
 class Category < ApplicationRecord
+  # そのカテゴリのスポットがある場合、カテゴリの削除時にエラー
+  has_many :spots, dependent: :restrict_with_error
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :display_order, presence: true, uniqueness: true
 end
