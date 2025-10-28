dd0bb5c5310f6d28d05ddf7bf1737bae14a7bdde
kozaken <gkozaka4@gmail.com>
2025-10-28 11:58:38 +0900

    add:spotsマイグレーションの設定とマイグレーションの実行

--- test/factories/spots.rb
+++ test/factories/spots.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: spots
+#
+#  id              :bigint           not null, primary key
+#  address         :text
+#  name            :string(50)       not null
+#  phone_number    :string(20)
+#  website_url     :text
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  card_id         :bigint           not null
+#  google_place_id :string
+#
+# Indexes
+#
+#  index_spots_on_card_id          (card_id)
+#  index_spots_on_google_place_id  (google_place_id) UNIQUE WHERE (google_place_id IS NOT NULL)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (card_id => cards.id)
+#
 FactoryBot.define do
   factory :spot do
     
